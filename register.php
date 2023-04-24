7dea22bc5ebb85f79d26c88cb69b69ec7cd5b5ee
Romack Natividad <romacknatividad@gmail.com>
2023-04-25 05:13:45 +0800

    Users table testers

---
+++ register.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Registration</title>
+</head>
+<body>
+<h1>Register a User</h1>
+
+<form action="save-registration.php" method="POST">
+	<div>
+		<label>First Name</label>
+		<input type="text" name="first_name" placeholder="First Name" />	
+	</div>
+	<div>
+		<label>Last Name</label>
+		<input type="text" name="last_name" placeholder="Last Name" />	
+	</div>
+	<div>
+		<label>Email Address</label>
+		<input type="email" name="email" placeholder="email@address.com" />	
+	</div>
+	<div>
+		<label>Password</label>
+		<input type="password" name="password" />	
+	</div>
+	<div>
+		<label>Password</label>
+		<input type="password" name="confirm_password" />	
+	</div>
+	<div>
+		<button>
+			Register User
+		</button>	
+	</div>
+</form>
+</body>
+</html>
---
+++ src/User.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App;
+
+use PDO;
+
+class User
+{
+	protected $id;
+	protected $first_name;
+	protected $last_name;
+	protected $email;
+	protected $pass;
+	protected $created_at;
+
+	public function getFullName()
+	{
+		return $this->first_name . ' ' . $this->last_name;
+	}
+
+	public function getFirstName()
+	{
+		return $this->first_name;
+	}
+
+	public function getLastName()
+	{
+		return $this->last_name;
+	}
+
+	public function getEmail()
+	{
+		return $this->email;
+	}
+
+	public static function attemptLogin($email, $pass)
+	{
+		global $conn;
+
+		try {
+			$sql = "
+				SELECT * FROM users
+				WHERE email=:email
+					AND pass=:pass
+				LIMIT 1
+			";
+			$statement = $conn->prepare($sql);
+			$statement->execute([
+				'email' => $email,
+				'pass' => $pass
+			]);
+			$result = $statement->fetchObject('App\User');
+			return $result;
+		} catch (PDOException $e) {
+			error_log($e->getMessage());
+		}
+
+		return null;
+	}
+
+	public static function register($first_name, $last_name, $email, $password)
+	{
+		global $conn;
+
+		try {
+			$sql = "
+				INSERT INTO users (first_name, last_name, email, pass)
+				VALUES ('$first_name', '$last_name', '$email', '$password')
+			";
+			$conn->exec($sql);
+			// echo "<li>Executed SQL query " . $sql;
+			return true;
+		} catch (PDOException $e) {
+			error_log($e->getMessage());
+		}
+
+		return false;
+	}
+
+	public static function registerMany($users)
+	{
+		global $conn;
+
+		try {
+			foreach ($users as $user) {
+				$sql = "
+					INSERT INTO users
+					SET
+						first_name=\"{$user['first_name']}\",
+						last_name=\"{$user['last_name']}\",
+						email=\"{$user['email']}\",
+						pass=\"{$user['pass']}\"
+				";
+				$conn->exec($sql);
+				// echo "<li>Executed SQL query " . $sql;
+			}
+			return true;
+		} catch (PDOException $e) {
+			error_log($e->getMessage());
+		}
+
+		return false;
+	}	
+}
